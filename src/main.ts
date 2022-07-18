2f78606494ebde5e3bbbc2f9540680f4b809a636
Alejandro Jauregui <alejandrojauregui12@yahoo.es>
2022-07-17 21:52:15 -0400

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { initSwagger } from './app.swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
   initSwagger(app);
 
